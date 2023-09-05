da5b6294b3d8e16ef8924095da9ea11fc6efe460
root <root@localhost>
2023-09-05 23:00:50 +0000

    done with part 1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,24 +5,53 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render('new') # New action is not being called here
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:success] = "Book edited successfully."
+      redirect_to book_path(@book)
+    else
+      flash[:error] = "Cannot update book information."
+      render('edit')
+    end
   end
 
   def delete
+    @book = Book.find(params[:id])
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:success] = "Book deleted successfully."
+    else
+      flash[:error] = "Unable to delete book -- an error was encountered."
+    end
+    redirect_to books_path
+  end
+
+  def book_params
+    params.require(:book).permit(:name, :author, :description, :price)
   end
   
 end
